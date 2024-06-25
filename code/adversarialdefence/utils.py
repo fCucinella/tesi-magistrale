5a4a5a8e2f3bd9338d80a20631df1c0ff751f0d7
Francesco Cucinella <francesco_cucinella@MacBook-Air-di-Francesco-4.local>
2024-06-25 15:06:41 +0200

    modifica utils

--- code/adversarialdefence/utils.py
+++ code/adversarialdefence/utils.py
@@ -35,74 +35,6 @@ class GeneralUtils():
         shuffled_a[permutation] = a[index_permutation]
         return shuffled_a
 
-    def shuffle_in_uni(self, a, b):
-        assert len(a) == len(b)
-        shuffled_a = np.empty(a.shape, dtype=a.dtype)
-        shuffled_b = np.empty(b.shape, dtype=b.dtype)
-        length = len(a)
-        permutation = np.random.permutation(length)
-        index_permutation = np.arange(length)
-        shuffled_a[permutation] = a[index_permutation]
-        shuffled_b[permutation] = b[index_permutation]
-        return shuffled_a, shuffled_b
-
-    def shuffle_in_uni_with_permutation(self, a, b, permutation):
-        assert len(a) == len(b)
-        shuffled_a, shuffled_b = a.copy(), b.copy()
-        shuffled_permutation = self.shuffle(permutation)
-        shuffled_a[shuffled_permutation] = a[permutation]
-        shuffled_b[shuffled_permutation] = b[permutation]
-        return shuffled_a, shuffled_b
-
-    '''
-    SMM stands for source-level mutated model 
-    This function looks quite terrible and messy, should be simplified
-    '''
-    def print_messages_SMO(self, mode, train_datas=None, train_labels=None, mutated_datas=None, mutated_labels=None, model=None, mutated_model=None, mutation_ratio=0):
-        if mode in ['DR', 'DM']:
-            print('Before ' + mode)
-            print('Train data shape:', train_datas.shape)
-            print('Train labels shape:', train_labels.shape)
-            print('')
-
-            print('After ' + mode + ', where the mutation ratio is', mutation_ratio)
-            print('Train data shape:', mutated_datas.shape)
-            print('Train labels shape:', mutated_labels.shape)
-            print('')
-        elif mode in ['LE', 'DF', 'NP']:
-            pass 
-        elif mode in ['LR', 'LAs', 'AFRs']:
-            print('Original untrained model architecture:')
-            model.summary()
-            print('')
-
-            print('Mutated untrained model architecture:')
-            mutated_model.summary()
-            print('')
-        else:
-            pass
-    
-    '''
-    MMM stands for model-level mutated model 
-    '''
-    def print_messages_MMM_generators(self, mode, network=None, test_datas=None, test_labels=None, model=None, mutated_model=None, STD=0.1, mutation_ratio=0):
-        if mode in ['GF', 'WS', 'NEB', 'NAI', 'NS']:
-            print('Before ' + mode)
-            network.evaluate_model(model, test_datas, test_labels)
-            print('After ' + mode + ', where the mutation ratio is', mutation_ratio)
-            network.evaluate_model(mutated_model, test_datas, test_labels, mode)
-        elif mode in ['LD', 'LAm', 'AFRm']:
-            print('Before ' + mode)
-            model.summary()
-            network.evaluate_model(model, test_datas, test_labels)
-
-            print('After ' + mode)
-            mutated_model.summary()
-            network.evaluate_model(mutated_model, test_datas, test_labels, mode)
-        else:
-            pass
-    
-
     def get_data_with_advs(advs_path):
         df_advs = pd.read_csv(advs_path).assign(Label=1)
         df_advs.drop(df_advs.columns[0], axis=1, inplace=True)
@@ -125,12 +57,6 @@ class ModelUtils():
     def __init__(self):
         pass
 
-    def print_layer_info(self, layer):
-        layer_config = layer.get_config()
-        print('Print layer configuration information:')
-        for key, value in layer_config.items():
-            print(key, value)
-
     def model_copy(self, model, mode=''):
         original_layers = [l for l in model.layers]
         suffix = '_copy_' + mode 
@@ -149,36 +75,6 @@ class ModelUtils():
             for index in indices:
                 booleans_for_layers[index] = True
         return booleans_for_layers 
-
-    def print_comparision_of_layer_weights(self, old_model, new_model):
-        old_layers = [l for l in old_model.layers]
-        new_layers = [l for l in new_model.layers]
-        assert len(old_layers) == len(new_layers)
-        num_of_layers = len(old_layers)
-        booleans_for_layers = np.full(num_of_layers, True)
-        names_for_layers = []
-        for index in range(num_of_layers):
-            old_layer, new_layer = old_layers[index], new_layers[index]
-            names_for_layers.append(type(old_layer).__name__)
-            old_layer_weights, new_layer_weights = old_layer.get_weights(), new_layer.get_weights()
-            if len(old_layer_weights) == 0:
-                continue
-
-            is_equal_connections = np.array_equal(old_layer_weights[0], new_layer_weights[0])
-            is_equal_biases = np.array_equal(old_layer_weights[1], new_layer_weights[1])
-            is_equal = is_equal_connections and is_equal_biases
-            if not is_equal:
-                booleans_for_layers[index] = False 
-        
-        print('Comparision of weights between original model and mutated model,')
-        print('If the weights of specific layer is modified, return True. Otherwise, return False')
-        print('')
-        print(' Layer index |   Layer name   | Is mutated ?')
-        print(' -------------------------------------------')
-        for index, result in enumerate(booleans_for_layers):
-            name = names_for_layers[index]
-            print(' {index} | {name} | {result}'.format(index=str(index).rjust(11), name=name.rjust(14), result=(not result)))
-        print('')
     
     def binary_preds_supervised(model, X, threshold=0.5):
         try:
@@ -217,10 +113,6 @@ class ExaminationalUtils():
         assert mutation_ratio >= 0, 'Mutation ratio attribute should in the range [0, 1]'
         assert mutation_ratio <= 1, 'Mutation ratio attribute should in the range [0, 1]'
         pass 
-
-    def training_dataset_consistent_length_check(self, lst_a, lst_b):
-        assert len(lst_a) == len(lst_b), 'Training datas and labels should have the same length'
-        pass
 
     def valid_indices_of_mutated_layers_check(self, num_of_layers, indices):
         if indices is not None:
